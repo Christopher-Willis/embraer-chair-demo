b25d1f8df1a84a31dae9a0996184d72cd079b1c5
chrisw <chrisw@f1payments.com>
2022-09-10 11:15:58 -0500

    scaffolding build

--- src/App.js
+++ src/App.js
@@ -1,23 +1,43 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div id='mainPage' style={{ margin: '20px', display: 'flex' }}>
+        <div style={{ flexGrow: 1 }}>
+          heroDiv
+        </div>
+        <div id='sideBar' style={{ width: 'fit-content', display: 'flex', flexDirection: 'column', maxWidth: '300px' }}>
+          <div id='information'>
+            <h3 className='title'>
+              PARADIGMA CHAIR
+            </h3>
+            <div class='infoBox'>
+                Lots of information about the style and other stuff too, Lots of information about the style and other stuff too
+            </div>
+            <div class='infoBoxSmall'>
+                Cost
+            </div>
+            <div class='infoBoxSmall'>
+                Estimated Delivery
+            </div>
+          </div>
+          <div id='controls'>
+            <div className='dropdown'>
+              Style
+            </div>
+            <div className='dropdown'>
+              Color Palette
+            </div>
+            <div className='dropdown'>
+              Quantity
+            </div>
+            <div className='button'>
+              Continue To Payment
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
