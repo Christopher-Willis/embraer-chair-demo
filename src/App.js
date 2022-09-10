7e7f08fd3a07027980404cc6d11193c58538df1d
chrisw <chrisw@f1payments.com>
2022-09-10 15:04:45 -0500

    Dropdown started static hero image used

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
+import { useEffect, useState } from 'react';
 import './App.css';
+import imageThingy from './images/Lauderdale_Premium_Arms.png'
+
 
 function App() {
+  const [style, setStyle] = useState('_Premium_Arms');
+  const [color, setColor] = useState('Lauderdale_Premium_Arms');
+
+  const [imageName, setImageName] = useState(`./images/${color}${style}.png`);
+
+
+
+  useEffect(() => {
+    document.title = "Demo Page";  
+  }, []);
+
   return (
     <div className="App">
       <div id='mainPage' style={{ margin: '20px', display: 'flex' }}>
-        <div style={{ flexGrow: 1 }}>
-          heroDiv
+        <div style={{ flexGrow: 1, display: 'flex', justifyContent: 'center' }}>
+          {imageName && <img src={imageThingy} alt="thingy" style={{ height: '100vh' }}/>}
         </div>
         <div id='sideBar' style={{ width: 'fit-content', display: 'flex', flexDirection: 'column', maxWidth: '300px' }}>
           <div id='information'>
@@ -25,9 +39,31 @@ function App() {
           <div id='controls'>
             <div className='dropdown'>
               Style
+              <div className='dropdownlist'>
+                style menu
+              </div>
             </div>
             <div className='dropdown'>
               Color Palette
+              <div className='dropdownlist'>
+                <ul>
+                    <li>
+                      Lauderdale
+                    </li>
+                    <li>
+                      Mist
+                    </li>
+                    <li>
+                      Praeterra
+                    </li>
+                    <li>
+                      Stellar
+                    </li>
+                    <li>
+                      Tribeca
+                    </li>
+                  </ul>
+              </div>
             </div>
             <div className='dropdown'>
               Quantity
